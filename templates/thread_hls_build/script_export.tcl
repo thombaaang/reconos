c4d6930a3f189708004b3dce6086f3a0e6c59dc3
Christoph Rüthing <ruething@mail.upb.de>
2015-07-01 15:23:00 +0200

    Added __RECONOS__ definition to HLS project

--- templates/thread_hls_build/script_export.tcl
+++ templates/thread_hls_build/script_export.tcl
@@ -2,9 +2,9 @@
 
 open_project hls
 set_top rt_imp
-add_files reconos_calls.h -cflags "-D__SYNTHESIS__"
-add_files reconos_thread.h -cflags "-D__SYNTHESIS__"
-add_files [ glob *.cpp ] -cflags "-D__SYNTHESIS__"
+add_files reconos_calls.h -cflags "-D__SYNTHESIS__ -D__RECONOS__"
+add_files reconos_thread.h -cflags "-D__SYNTHESIS__ -D__RECONOS__"
+add_files [ glob *.cpp ] -cflags "-D__SYNTHESIS__ -D__RECONOS__"
 open_solution sol
 set_part {<<PART>>}
 create_clock -period <<CLKPRD>> -name default
